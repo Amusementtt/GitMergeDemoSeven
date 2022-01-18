ca2c71f2714ff40718c93c1367f24cbb31231a76
王钰 <wangyu9@bonc.com.cn>
2022-01-18 17:06:18 +0800

    master:提交master分支

---
+++ src/main/java/com/example/gitmergedemoseven/GitMergeDemoSevenApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemoseven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergeDemoSevenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMergeDemoSevenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitmergedemoseven/GitMergeDemoSevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemoseven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMergeDemoSevenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
